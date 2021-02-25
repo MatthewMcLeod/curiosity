9966488ba3e172c29670cff3035230de8456e075
Matthew Schlegel <mkschleg@gmail.com>
2021-02-25 13:49:41 -0700

    Added mountain car.

--- src/Curiosity.jl
+++ src/Curiosity.jl
@@ -2,26 +2,25 @@ module Curiosity
 
 abstract type Learner end
 
+@reexport using MinimalRLCore
+
 export TB, TBAuto, update!
 include("learners/TB.jl")
 include("learners/TB_Auto.jl")
 
-
 export TabularRoundRobin, update!
 include("learners/TabularRoundRobin.jl")
 
-
 export Agent, agent_end!, step!
 include("agent/agent.jl")
-
 
 abstract type CumulantSchedule end
 function update! end
 function get_cumulants end
 
-export TabularTMaze, env_step!, env_start!, valid_state_mask
-    # TabularMazeCumulantSchedules, get_cumulants, update!
+export TabularTMaze, MountainCar, valid_state_mask
 include("environments/tabular_tmaze.jl")
+include("environments/mountain_car.jl")
 
 #utils
 include("utils/tmaze.jl")
