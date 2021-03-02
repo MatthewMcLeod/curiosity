9ef74194af0f9dd89dacc6f8ec93ba1e20756fc8
MatthewMcLeod <mkmmcleod@gmail.com>
2021-03-02 15:30:08 -0500

    Plotting of Mountain Car GVFs

--- src/gen_mountain_car_eval_set.jl
+++ src/gen_mountain_car_eval_set.jl
@@ -1,6 +1,7 @@
 using Curiosity
 using MinimalRLCore
 using Statistics
+using GVFHordes
 
 include("../experiments/mountain_car.jl")
 
@@ -10,25 +11,52 @@ horde = MountainCarExperiment.get_horde(parsed)
 normalized = true
 env = MountainCar(0.0,0.0, normalized)
 
-num_start_states = 500
-
-start_states = []
-
-for i in 1:num_start_states
-    MinimalRLCore.reset!(env)
-    s = MinimalRLCore.get_state(env)
-    push!(start_states,s)
-end
 num_returns = 1
 γ_thresh=1e-6
-rets = monte_carlo_returns(env, horde.gvfs[1], start_states, num_returns, γ_thresh)
 
 using Plots
 pyplot()
+task_gvf = Curiosity.MountainCarUtils.task_gvf()
 
-x = [x for (x,y) in start_states]
-y = [y for (x,y) in start_states]
+gvfs = [horde.gvfs..., task_gvf]
+#NOTE: GENERATE SCATTER
+# num_start_states = 500
+#
+# start_states = []
+#
+# for i in 1:num_start_states
+#     MinimalRLCore.reset!(env)
+#     s = MinimalRLCore.get_state(env)
+#     push!(start_states,s)
+# end
+# for (gvf_i,gvf) in enumerate(horde.gvfs)
+#     rets = monte_carlo_returns(env, gvf, start_states, num_returns, γ_thresh)
+#
+#     x = [x for (x,y) in start_states]
+#     y = [y for (x,y) in start_states]
+#
+#     rets = mean(rets, dims = 2)
+#     rets = collect(Iterators.flatten(rets))
+#     scatter(x,rets, legend=false, ylabel="Cumulant Val", xlabel="Starting X Pos", title = "GVF: $( gvf_i)")
+#     savefig("./MC_gvf_$(gvf_i).png")
+# end
 
-rets = mean(rets, dims = 2)
+
+for (gvf_i,gvf) in enumerate(gvfs)
+# Create heatmap version
+increment = 0.01
+vel_limit = (0, 1)
+pos_initial_range = (0.4, 0.9)
+
+ys = collect(vel_limit[1]:increment:vel_limit[2])
+xs = collect(pos_initial_range[1]:increment:pos_initial_range[2])
+
+start_states = [[x,y] for x in xs for y in ys]
+rets = monte_carlo_returns(env, gvf, start_states, num_returns, γ_thresh)
 rets = collect(Iterators.flatten(rets))
-scatter(vec(x),vec(rets), legend=false, ylabel="Cumulant Val", xlabel="Starting X Pos")
+ret_heatmap = reshape(rets,length(xs),length(ys))
+
+surface(xs,ys,ret_heatmap', xlabel=" Starting X position", ylabel = "Starting Velocity", title = "GVF: $( gvf_i)")
+savefig("./MC_gvf_$(gvf_i).png")
+
+end
--- src/utils/mountain_car.jl
+++ src/utils/mountain_car.jl
@@ -4,12 +4,16 @@ using GVFHordes
 const discount = 0.99
 import ..MountainCarConst
 
-# function MCNorm(obs)
-#     pos_limit = MountainCarConst.pos_limit
-#     vel_limit = MountainCarConst.vel_limit
-#     return Float32[(obs[1] - pos_limit[1])/(pos_limit[2] - pos_limit[1]),
-#                    (obs[2] - vel_limit[1])/(vel_limit[2] - vel_limit[1])]
-# end
+function MCNorm(obs)
+    pos_limit = MountainCarConst.pos_limit
+    vel_limit = MountainCarConst.vel_limit
+    return Float32[(obs[1] - pos_limit[1])/(pos_limit[2] - pos_limit[1]),
+                   (obs[2] - vel_limit[1])/(vel_limit[2] - vel_limit[1])]
+end
+
+function task_gvf()
+    GVF(GVFParamFuncs.FunctionalCumulant(task_cumulant), GVFParamFuncs.StateTerminationDiscount(0.9, task_pseudoterm, 0.0), GVFParamFuncs.FunctionalPolicy(energy_pump_policy) )
+end
 
 function steps_to_wall_gvf()
     GVF(GVFParamFuncs.FunctionalCumulant(steps_to_wall_cumulant), GVFParamFuncs.StateTerminationDiscount(discount, steps_to_wall_pseudoterm, 0.0), GVFParamFuncs.FunctionalPolicy(energy_pump_policy) )
@@ -52,6 +56,17 @@ function steps_to_goal_cumulant(obs, action, pred)
     normed_goal_pos = 1.0
     # return obs[1] >= MountainCarConst.pos_limit[2] ? 1.0 : 0.0
     return obs[1] >= normed_goal_pos ? 1.0 : 0.0
+end
+
+function task_pseudoterm(obs)
+    normed_goal_pos = 1.0
+    return obs[1] >= normed_goal_pos
+end
+
+function task_cumulant(obs, action, pred)
+    normed_goal_pos = 1.0
+    # return obs[1] >= MountainCarConst.pos_limit[2] ? 1.0 : 0.0
+    return obs[1] >= normed_goal_pos ? 0.0 : -1.0
 end
 
 end #end MountainCarUtils
