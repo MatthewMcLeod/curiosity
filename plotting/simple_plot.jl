6ff485b93f8b92b144ee06bfbfdc9c9b597c6d1a
MatthewMcLeod <mkmmcleod@gmail.com>
2021-03-03 16:31:55 -0500

    Mountain Car takes a long time to run

---
+++ plotting/simple_plot.jl
@@ -0,0 +1,8 @@
+using Reproduce
+
+tst = ItemCollection("/home/matthewmcleod/Documents/Masters/curiosity/MountainCar/data")
+
+
+function load_results(ic, param)
+    FileIO.load(joinpath(itm.folder_str, "results.jld2")))
+end
