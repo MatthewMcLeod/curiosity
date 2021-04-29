ec3693b453e1772bd462ae88393a182b74e78032
MatthewMcLeod <mkmmcleod@gmail.com>
2021-04-29 19:10:58 -0400

    CC sweep changed wrong parallel config

--- parallel/onedtmaze_control/ESARSA.jl
+++ parallel/onedtmaze_control/ESARSA.jl
@@ -1,15 +1,15 @@
 #!/cvmfs/soft.computecanada.ca/easybuild/software/2020/avx2/Core/julia/1.5.2/bin/julia
 #SBATCH --mail-user=mmcleod2@ualberta.ca
 #SBATCH --mail-type=ALL
-#SBATCH -o onedtmaze_GPI_no_reward.out # Standard output
-#SBATCH -e onedtmaze_GPI_no_reward.err # Standard error
-#SBATCH --mem-per-cpu=4000M # Memory request of 3GB
-#SBATCH --time=05:00:00 #
-#SBATCH --ntasks=20
+#SBATCH -o onedtmaze_ESARSA_Control.out # Standard output
+#SBATCH -e onedtmaze_ESARSA_Control.err # Standard error
+#SBATCH --mem-per-cpu=6000M # Memory request of 3GB
+#SBATCH --time=02:00:00 #
+#SBATCH --ntasks=16
 #SBATCH --account=def-amw8
 
 using Pkg
 Pkg.activate(".")
 
 include(joinpath(ENV["SLURM_SUBMIT_DIR"], "parallel/parallel_config.jl"))
-reproduce_config_experiment("configs/revised_onedtmaze/GPI_behaviour_no_reward.toml", save_path="~/scratch/curiosity")
+reproduce_config_experiment("configs/onedtmaze_control/ESARSA_behaviour.toml", save_path="~/scratch/curiosity")
