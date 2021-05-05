e92136e04ef8dc82d69a9ea70274afdb8a8af3f6
chunloklo <clo42@gatech.edu>
2021-05-05 12:33:35 -0400

    testing out CC with copies of experiment files

---
+++ parallel/onedtmaze_control/chunlok_CC_please_GPI_Auto_Part_1.jl
@@ -0,0 +1,15 @@
+#!/cvmfs/soft.computecanada.ca/easybuild/software/2020/avx2/Core/julia/1.5.2/bin/julia
+#SBATCH --mail-user=chunlok@ualberta.ca
+#SBATCH --mail-type=ALL
+#SBATCH -o onedtmaze_GPI_Auto_Part_1.out # Standard output
+#SBATCH -e onedtmaze_GPI_Auto_Part_1.err # Standard error
+#SBATCH --mem-per-cpu=4000M # Memory request of 3GB
+#SBATCH --time=00:30:00 #
+#SBATCH --ntasks=25
+#SBATCH --account=def-whitem
+
+using Pkg
+Pkg.activate(".")
+
+include(joinpath(ENV["SLURM_SUBMIT_DIR"], "parallel/parallel_config.jl"))
+reproduce_config_experiment("configs/onedtmaze_control/GPI_part_1_Auto_Demons.toml", save_path="~/scratch/curiosity")
