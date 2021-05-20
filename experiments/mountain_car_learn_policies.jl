a6c7be04cd6473cb15b0ec117c95ba82a757d787
MatthewMcLeod <mkmmcleod@gmail.com>
2021-05-19 21:53:16 -0400

    Updating learned policies - WIP

---
+++ experiments/mountain_car_learn_policies.jl
@@ -0,0 +1,135 @@
+module MountainCarLearnPolicy
+
+using Curiosity, Statistics, Plots, FileIO, BSON
+using Random, StatsBase, RollingFunctions
+using GVFHordes, ProgressMeter
+
+const MCU = Curiosity.MountainCarUtils
+
+# Cumulant
+struct Reward end
+
+(c::Reward)(;kwargs...) = kwargs[:r]
+
+struct ConstantDiscount{F}
+    val::F
+end
+
+(d::ConstantDiscount)(;kwargs...) = d.val
+
+# Environment
+function construct_env()
+    normalized = true
+    env = MountainCar(0.0, 0.0, normalized)
+end
+
+# Agent
+function construct_agent(numtilings, numtiles, lu_str, α, λ, ϵ, γ)
+    obs_size = 2
+    fc = Curiosity.SparseTileCoder(numtilings, numtiles, obs_size)
+    feature_size = size(fc)
+
+    lu = if lu_str == "ESARSA"
+        ESARSA(lambda=λ, opt=Curiosity.Descent(α))
+    elseif lu_str == "SARSA"
+        SARSA(lambda=λ, opt=Curiosity.Descent(α))
+    elseif lu_str == "TB"
+        TB(lambda=λ, opt=Curiosity.Descent(α))
+    else
+        throw(ArgumentError("$(lu_str) Not a valid behaviour learning update"))
+    end
+#     (update, num_features, num_actions, num_demons, w_init)
+    learner = LinearQLearner(lu, feature_size, 3, 1,0)
+    exploration = EpsilonGreedy(ϵ)
+    cumulant = Reward()
+    # discount = ConstantDiscount(γ)
+    discount = GVFParamFuncs.ConstantDiscount(γ)
+
+    b_gvf = make_behaviour_gvf(learner, discount, fc, exploration)
+    b_demons = Horde([b_gvf])
+
+    Curiosity.PolicyLearner(learner,
+                            fc,
+                            exploration,
+                            discount,
+                            cumulant,
+                            zeros(2),
+                            0,
+                            b_demons)
+end
+
+function make_behaviour_gvf(behaviour_learner, discount, fc, exploration_strategy)
+    function b_π(state_constructor, learner, exploration_strategy; kwargs...)
+        s = state_constructor(kwargs[:state_t])
+        preds = learner(s)
+        return exploration_strategy(preds)[kwargs[:action_t]]
+    end
+    GVF_policy = GVFParamFuncs.FunctionalPolicy((;kwargs...) -> b_π(fc, behaviour_learner, exploration_strategy; kwargs...))
+    BehaviourGVF = GVF(GVFParamFuncs.RewardCumulant(),discount, GVF_policy)
+    return BehaviourGVF
+end
+
+function main_experiment(;progress=true)
+    # Learn the policy
+    seed = 1029
+    Random.seed!(seed)
+    numtilings, numtiles = 8, 8
+    lu_str = "TB"
+    α = 0.1/numtilings
+    λ = 0.9
+    ϵ = 0.1
+    γ = 0.99
+
+    info = Dict(
+        "seed"=>seed,
+        "numtilings"=>numtilings,
+        "numtiles"=>numtiles,
+        "lu"=>"lu_str",
+        "α"=>α,
+        "λ"=>λ,
+        "ϵ"=>ϵ,
+        "γ"=>γ,
+        "rew"=>"Env"
+    )
+
+
+    env = construct_env()
+
+    agent = construct_agent(numtilings, numtiles, lu_str, α, λ, ϵ, γ)
+
+    steps = Int[]
+    ret = Float64[]
+    max_num_steps = 1000
+    eps = 0
+    while sum(steps) < max_num_steps
+    is_terminal = false
+
+    max_episode_steps = min(max_num_steps - sum(steps), 1000)
+    s = start!(env)
+    a = start!(agent, s)
+    stp = 0
+    a = 0
+    tr = 0.0
+    prg_bar = ProgressMeter.Progress(num_steps, "Step: ")
+
+    while !is_terminal && stp <= max_episode_steps
+        s, r, is_terminal = MinimalRLCore.step!(env, a)
+        a = MinimalRLCore.step!(agent, s, r, is_terminal)
+        tr += r
+        stp += 1
+    end
+
+    push!(steps, stp)
+    push!(ret, tr)
+
+    eps += 1
+
+    if progress
+        next!(prg_bar)
+    end
+    end
+
+    Curiosity.save(agent, "policy.bson", info)
+end
+
+end
--- src/LearnedPolicy.jl
+++ src/LearnedPolicy.jl
@@ -22,22 +22,23 @@ Base.get(π::LearnedPolicy, state_t, action_t) = π(state_t, action_t)
 (lp::LearnedPolicy)(s, a) = lp.policy(s)[a]
 
 # Policy learning struct
-mutable struct PolicyLearner{L<:Learner, FC, E, D, C, Φ} <: MinimalRLCore.AbstractAgent
+mutable struct PolicyLearner{L<:Learner, FC, E, D, C, Φ, DEMON} <: MinimalRLCore.AbstractAgent
     learner::L
     fc::FC
     exploration::E
-    
+
     discount::D
     cumulant::C
     o_t::Φ
     a_t::Int
+    BehaviourDemon::DEMON
 end
 
 function MinimalRLCore.start!(pl::PolicyLearner, o)
     s_t = pl.fc(o)
     pl.o_t = copy(o)
     pl.a_t = μ(pl, s_t)
-    
+
     pl.a_t
 end
 
@@ -54,34 +55,33 @@ end
 function MinimalRLCore.step!(pl::PolicyLearner, o_tp1, rew, term)
     s_tp1 = pl.fc(o_tp1)
     next_action = μ(pl, s_tp1)
-    
+
     rew = pl.cumulant(;r=rew, o_t=pl.o_t, a_t=pl.a_t, o_tp1=o_tp1)
-    γ = if pl.discount isa Number
-        pl.discount
-    else
-        pl.discount(;r=rew, o_t=pl.o_t, a_t=pl.a_t, o_tp1=o_tp1)
-    end
-    
+    # γ = if pl.discount isa Number
+    #     pl.discount
+    # else
+    #     pl.discount(;r=rew, o_t=pl.o_t, a_t=pl.a_t, o_tp1=o_tp1)
+    # end
+
     update!(pl.learner,
-            nothing,
+            pl.BehaviourDemon,
             pl.o_t,
             o_tp1,
             pl.fc(pl.o_t),
             pl.a_t,
-            s_tp1,
+            pl.fc(o_tp1),
             next_action,
             term,
-            [rew],
-            [γ],
-            (state, obs) -> μ_dist(pl, state))
-    
+            (state, obs) -> μ_dist(pl, state),
+            rew)
+
     pl.o_t = copy(o_tp1)
     pl.a_t = next_action
-    
+
     pl.a_t
 end
 
-save(pl::PolicyLearner, loc, info::AbstractDict) = 
+save(pl::PolicyLearner, loc, info::AbstractDict) =
     BSON.bson(loc,
               Dict("learner"=>pl.learner,
                    "fc"=>pl.fc,
