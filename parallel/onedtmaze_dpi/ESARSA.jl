f213601eb01acb6088e524bdd3761890983c09a1
MatthewMcLeod <mkmmcleod@gmail.com>
2021-05-19 23:37:59 -0400

    config fix

--- parallel/onedtmaze_dpi/ESARSA.jl
+++ parallel/onedtmaze_dpi/ESARSA.jl
@@ -1,8 +1,8 @@
-#!/cvmfs/soft.computecanada.ca/easybuild/software/2020/avx2/Core/julia/1.5.2/bin/julia
+#!/cvmfs/soft.computecanada.ca/easybuild/software/2020/avx2/Core/julia/1.6.0/bin/julia
 #SBATCH --mail-user=mmcleod2@ualberta.ca
 #SBATCH --mail-type=ALL
-#SBATCH -o onedtmaze_control_esarsa_dpi.out # Standard output
-#SBATCH -e onedtmaze_control_esarsa_dpi.err # Standard error
+#SBATCH -o onedtmaze_esarsa.out # Standard output
+#SBATCH -e onedtmaze_esarsa.err # Standard error
 #SBATCH --mem-per-cpu=4000M # Memory request of 3GB
 #SBATCH --time=02:00:00 #
 #SBATCH --ntasks=32
--- parallel/onedtmaze_dpi/GPI.jl
+++ parallel/onedtmaze_dpi/GPI.jl
@@ -1,8 +1,8 @@
 #!/cvmfs/soft.computecanada.ca/easybuild/software/2020/avx2/Core/julia/1.6.0/bin/julia
 #SBATCH --mail-user=mmcleod2@ualberta.ca
 #SBATCH --mail-type=ALL
-#SBATCH -o onedtmaze_GPI_dpi.out # Standard output
-#SBATCH -e onedtmaze_GPI_dpi.err # Standard error
+#SBATCH -o onedtmaze_GPI.out # Standard output
+#SBATCH -e onedtmaze_GPI.err # Standard error
 #SBATCH --mem-per-cpu=4000M # Memory request of 3GB
 #SBATCH --time=05:00:00 #
 #SBATCH --ntasks=64
